3f303e05eacf911afe283a28997319167bf1c326
DanDeanDean <dpd5518@psu.edu>
2024-03-14 22:12:01 -0400

    added some c

---
+++ c-src/main.c
@@ -0,0 +1,9 @@
+// #include <CoreGraphics/CoreGraphics.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  for (int i = 0; i < argc; i++) {
+    printf("%s ", argv[i]);
+  }
+  printf("\n");
+}
