ada149fa4ac11a7d91a837e1c9ad072a29ef6b2c
dandeandean <dpd5518@psu.edu>
2024-04-14 14:23:12 -0400

    proto use after free

--- c-src/use-after-free/main.c
+++ c-src/use-after-free/main.c
@@ -9,11 +9,32 @@ struct String {
 
 int main(int argc, char **argv) {
   struct String *s = malloc(sizeof(struct String));
-  s->s = "hello";
+  if (argc < 2) {
+    printf("Usage: ./run <string>\n");
+    return 1;
+  }
+  s->s = argv[1];
   s->len = strlen(s->s);
-  printf("%s\n", s->s);
-  printf("Freeing s\n");
-  free(s);
-  printf("%s\n", s->s);
+  printf("String(%s,%d)\n", s->s,s->len);
+  char option;    
+  while (option != 'q') {
+    printf("What to do next?\n");
+    printf("(f)ree\n(p)rint\n(q)uit\n>");
+    scanf("%c",&option);
+    switch (option)
+    {
+    case 'f':
+      system("clear");
+      printf("\nFreeing...\n");
+      free(s);
+      break;
+    case 'p':
+      system("clear");
+      printf("\nString(%s,%d)\n\n", s->s,s->len);
+      break;
+    default:
+      break;
+    }
+  }
   return 0;
 }
