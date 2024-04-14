c7280b83b022596c1179af5071770f142bd9822b
dandeandean <dpd5518@psu.edu>
2024-04-14 13:59:12 -0400

    add guessing game

--- c-src/buffer-overflow/main.c
+++ c-src/buffer-overflow/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-// #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BUF 5
+#define MAX_BUF 20
 
 int main(int argc, char **argv) {
+  const char secret[] = "letmein";
   char buf[MAX_BUF];
+  if (argc < 2){
+    printf("Usage: ./run <guess>\n");
+    return 1;
+  }
   if (argc > 1) {
     strcpy(buf, argv[1]);
   }
-  printf("\n");
+  if (0 == strncmp(buf,secret,sizeof(secret))) {
+    printf("You guessed correctly!\n");
+    printf("The secret was: %s \n",secret);
+    return 0;
+  }
+  printf("'%s' was not the secret.\nTry again later!\n",buf);
+  return 0;
 }
